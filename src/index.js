6e3fd04821f8b24697e40686cb40d9f1f5ac5efe
Dominic Egginton <dominic.egginton@gmail.com>
2021-02-11 20:35:41 +0000

    remove koa-sassy, koa-views & pug module dependencies

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@
 
 /* KOA MODULES */
 const Koa = require('koa')
-const sassy = require('koa-sassy')
-const views = require('koa-views')
 
 /* MODULES */
 const http = require('http')
-const path = require('path')
 
 /* GLOBALS */
 const PORT = process.env.PORT || 3000
@@ -15,8 +12,6 @@ const PORT = process.env.PORT || 3000
 /* KOA SETUP */
 const app = new Koa()
 app.server = http.createServer(app.callback())
-app.use(sassy(path.join(__dirname, '/sass')))
-app.use(views(path.join(__dirname, '/views'), { extension: 'pug' }))
 
 /* START SERVER */
 app.server.listen(PORT, () => {

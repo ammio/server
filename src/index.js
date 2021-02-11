2ddd2e7004c97648d004a7c1ba942c7fa97314be
Dominic Egginton <dominic.egginton@gmail.com>
2021-02-11 22:40:51 +0000

    added connection to mongo database

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@
 
 /* IMPORT MODULES */
 const Koa = require('koa')
+const MongoClient = require('mongodb').MongoClient
 const http = require('http')
 
 /* GLOBALS */
 const PORT = process.env.PORT || 3000
+const MONGO_URL = process.env.MONGO_URL || 'localhost'
+const MONGO_PORT = process.env.MONGO_PORT || 27017
 
 /* KOA SETUP */
 const app = new Koa()
 app.server = http.createServer(app.callback())
 
 /* START SERVER */
-app.server.listen(PORT, () => {
-  console.log(`ammio started on port ${PORT}`)
-})
+;(async () => {
+  const mongo = await MongoClient.connect(`mongodb://${MONGO_URL}:${MONGO_PORT}/`, { useUnifiedTopology: true })
+  app.context.database = mongo.db('ammio')
+  app.server.listen(3000,async () => console.log(`ammio started on port ${PORT}`))
+})()

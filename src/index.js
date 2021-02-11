578e7b976b473fbd4cf98f233f588bf9115ec04c
Dominic Egginton <dominic.egginton@gmail.com>
2021-02-11 22:41:39 +0000

    remove referance to redundent http module

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 /* IMPORT MODULES */
 const Koa = require('koa')
 const MongoClient = require('mongodb').MongoClient
-const http = require('http')
 
 /* GLOBALS */
 const PORT = process.env.PORT || 3000
@@ -12,11 +11,10 @@ const MONGO_PORT = process.env.MONGO_PORT || 27017
 
 /* KOA SETUP */
 const app = new Koa()
-app.server = http.createServer(app.callback())
 
 /* START SERVER */
 ;(async () => {
   const mongo = await MongoClient.connect(`mongodb://${MONGO_URL}:${MONGO_PORT}/`, { useUnifiedTopology: true })
   app.context.database = mongo.db('ammio')
-  app.server.listen(3000,async () => console.log(`ammio started on port ${PORT}`))
+  app.listen(3000, async () => console.log(`ammio started on port ${PORT}`))
 })()

e980e5d1145f515ee7875e62c4080fb2b360fda2
Dominic Egginton <dominic.egginton@gmail.com>
2021-02-15 15:06:14 +0000

    create analytics enpoints

---
+++ server/src/modules/analytics.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const ObjectID = require('mongodb').ObjectID
+
+class Analytics {
+  constructor (database) {
+    this.collection = database.collection('analytics')
+    return this
+  }
+
+  async log (client) {
+    if (client._id) {
+      const filter = { _id: ObjectID(client._id) }
+      let update
+      if (client.online === false) {
+        update = { $set: { online: client.online, duration: client.duration } }
+      } else {
+        client.online = new Date()
+        update = { $set: { online: client.online } }
+      }
+      const result = await this.collection.updateOne(filter, update)
+      const log = result
+      log.online = true
+      return true
+    }
+
+    client.online = new Date()
+    const result = await this.collection.insertOne(client)
+    const log = result.ops[0]
+    log.online = true
+    return log
+  }
+
+  async all () {
+    return await this.collection.find().toArray()
+  }
+}
+
+module.exports = Analytics
--- server/src/routes/analytics.js
+++ server/src/routes/analytics.js
@@ -2,11 +2,28 @@
 
 /* IMPORT MODULES */
 const Router = require('@koa/router')
+const Analytics = require('../modules/analytics')
 
 /* SETUP ROUTER */
 const router = new Router()
 router.prefix('/analytics')
 
 /* ROUTES */
+router.post('/', async ctx => {
+  const analytics = new Analytics(ctx.database)
+  const client = ctx.request.body
+  ctx.body = await analytics.log(client)
+})
 
+router.post('/online', async ctx => {
+  const analytics = new Analytics(ctx.database)
+  const client = JSON.parse(ctx.request.body)
+  const online = await analytics.log(client)
+  ctx.body = JSON.stringify(online)
+})
+
+router.get('/all', async ctx => {
+  const analytics = new Analytics(ctx.database)
+  ctx.body = await analytics.all()
+})
 module.exports = router

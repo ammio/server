16572299ad06f9f2096c2de2a5460902bee3a06f
Dominic Egginton <dominic.egginton@gmail.com>
2021-02-20 03:32:25 +0000

    add baasic button component

---
+++ client/src/components/partials/ammioButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="ammio-button">
+    <button v-bind="$attrs" @click="clicked">
+      <slot/>
+    </button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ammio-button',
+  inheritAttrs: false
+}
+</script>
+
+<style lang="sass" scoped>
+section.ammio-button
+  width: 200px
+  height: 40px
+  button
+    background: var(--color-primary)
+    position: relative
+    overflow: hidden
+    font-family: Open-Sans
+    font-weight: 1rem
+    cursor: pointer
+    width: 100%
+    height: 40px
+    border-radius: 12px
+    border: none
+    color: var(--color-text-alternative)
+    outline: none
+</style>
